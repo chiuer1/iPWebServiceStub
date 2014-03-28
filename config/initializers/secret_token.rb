bb4d1e031af4669ba9625dd58b4ac1ea1e0b919f
Eric Ho-Kee Chiu <eric.chiu@novartis.com>
2014-03-27 20:41:46 -0400

    Initial creation of artifacts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IPWebServiceStub::Application.config.secret_key_base = '0258dcf4aa5191c456c99d80b9fe606112d70058b5e21b97a5d3e030ac73303c698b3660f7ea9447b88c35b74a02d6fbab1632d6fae28e9d65d87e4d34dc0e5d'
